8decbed572e38b9d7c4a83a68721a32ec0aedae7
Scott Allen <scott@OdeToCode.com>
2018-05-07 17:01:44 -0400

    adding razor

---
+++ TrafficWeb/Controllers/VehicleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrafficWeb.Data;
+
+namespace TrafficWeb
+{
+    [Route("[controller]/[action]")]
+    public class VehicleController : Controller
+    {
+        private readonly ICarDb db;
+
+        public VehicleController(ICarDb db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+    }
+}
---
+++ TrafficWeb/Pages/CarList.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TrafficWeb.Data;
+using TrafficWeb.Model;
+
+namespace TrafficWeb.Pages
+{
+    public class CarListModel : PageModel
+    {
+        private readonly ICarDb db;
+
+        public IEnumerable<Car> Cars { get; set; }
+
+        public CarListModel(ICarDb db)
+        {
+            this.db = db;
+        }
+
+        public void OnGet()
+        {
+            Cars = db.GetAll();
+        }
+    }
+}
