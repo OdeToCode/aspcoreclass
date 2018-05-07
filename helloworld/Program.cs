289e3150f571dbc1929e727528f1b84d321883c3
Scott Allen <scott@OdeToCode.com>
2018-05-07 10:55:05 -0400

    hello world!

---
+++ helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+#if NET461
+            Console.WriteLine("Hello from NET 461");
+#else
+            Console.WriteLine("Hello from .NET Core!");
+#endif
+
+            Console.ReadLine();
+        }
+    }
+}
