ef459835948cf65ebec9a84177fa7849df820864
Scott Allen <scott@OdeToCode.com>
2018-05-30 04:27:19 -0400

    say hello

---
+++ Middleware/SayHelloMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspcoreclass.Middleware
+{
+    public static class AppBuilderExtensions
+    {
+        public static IApplicationBuilder UseGreeting(
+            this IApplicationBuilder app,
+            SayHelloOptions options)
+        {
+            app.UseMiddleware<SayHelloMiddleware>(options);
+            return app;
+        }
+    }
+
+
+    public class SayHelloOptions
+    {
+        public string Path { get; set; }
+        public string Greeting { get; set; }
+    }
+
+
+    public class SayHelloMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly SayHelloOptions options;
+
+        public SayHelloMiddleware(RequestDelegate next, 
+                                SayHelloOptions options)
+        {
+            this.next = next;
+            this.options = options;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments(options.Path))
+            {
+                context.Response.StatusCode = 200;
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync(options.Greeting);
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspcoreclass.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace aspcoreclass
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, 
+                            IHostingEnvironment env)
+        {
+
+            
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();              
+            }
+
+            app.UseStaticFiles();
+
+            var options = new SayHelloOptions
+            {
+                Path = "/sayhello",
+                Greeting = configuration["Greeting"]
+            };
+            app.UseGreeting(options);
+           
+         }
+    }
+}
