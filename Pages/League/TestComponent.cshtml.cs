575ca3cfef3668b5691768538a527e28a60b2954
Scott Allen <scott@OdeToCode.com>
2018-06-12 09:38:06 -0400

    adding a view component

---
+++ Pages/League/TestComponent.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspcoreclass.Pages.League
+{
+    public class TestComponentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ViewComponents/ListTeamsViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using aspcoreclass.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspcoreclass
+{
+    public class ListTeamsViewComponent : ViewComponent
+    {
+        private readonly ITeamData teamData;
+
+        public ListTeamsViewComponent(ITeamData teamData)
+        {
+            this.teamData = teamData;
+        }
+
+        public IViewComponentResult Invoke(string filter)
+        {
+            var model = teamData.GetAll().Where(t => t.Name.StartsWith(filter));
+            return View(model);
+        }
+    }
+}
